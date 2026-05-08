45b1b3833300cf5970b15ebd90ef21bc8a042a59
Deneth Koralage <denethkoralage@gmail.com>
2026-05-08 15:21:14 +0530

    chore: scaffold income repository

---
+++ backend.Application/Interfaces/IIncomeRepository.cs
@@ -0,0 +1,14 @@
+﻿using backend.Domain.Entities;
+
+namespace backend.Application.Interfaces;
+
+public interface IIncomeRepository
+{
+    Task<IEnumerable<Income>> GetAllIncomesAsync();
+    Task<Income?> GetIncomeByIdAsync(int id);
+    Task<Income> CreateIncomeAsync(Income income);
+    Task UpdateIncomeAsync(Income income);
+    Task DeleteIncomeAsync(int id);
+    Task<IEnumerable<Income>> GetIncomesByUserIdAsync(int userId);
+    Task<int> GetIncomesCountAsync();
+}
---
+++ backend.Infrastructure/Repositories/IncomeRepository.cs
@@ -0,0 +1,55 @@
+﻿using backend.Application.Interfaces;
+using backend.Domain.Entities;
+using backend.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace backend.Infrastructure.Repositories;
+
+public class IncomeRepository : IIncomeRepository
+{
+    private readonly AppDbContext _context;
+
+    public IncomeRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Income> CreateIncomeAsync(Income income)
+    {
+        _context.Incomes.Add(income);
+        await _context.SaveChangesAsync();
+        return income;
+    }
+
+    public Task DeleteIncomeAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Task<IEnumerable<Income>> GetAllIncomesAsync()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Task<Income?> GetIncomeByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Task<IEnumerable<Income>> GetIncomesByUserIdAsync(int userId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Task<int> GetIncomesCountAsync()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public Task UpdateIncomeAsync(Income income)
+    {
+        throw new NotImplementedException();
+    }
+}
